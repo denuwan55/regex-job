3fe42662c0fad98823870ad178c14b3503006a0e
denuwan55 <ldweerarathne5555@gmail.com>
2022-12-04 11:14:45 +0530

    initial commit

---
+++ code/test.py
@@ -0,0 +1,9 @@
+import re
+
+c = "Jazmine Holcomb 3212 Adams Avenue Washington MD eluir.azevedo.70@lucidmode.com"
+d = re.compile("^([A-Z][a-z]+) ([A-Z][a-z]+)")
+# d = re.compile("([A-Z][a-z]+) ([A-Z][a-z]+) ([A-Z][a-z]+) ([A-Z]+)")
+# d = re.compile("[A-Za-z0-9\.\-+_]+@[A-Za-z0-9\.\-+_]+\.[a-z]+")
+
+a = re.findall(d, c)
+print(a)
